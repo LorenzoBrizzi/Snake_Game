9e5a9f78a3a910641623ba374a913e0afa2878a1
LorenzoBrizzi <brizzi.lorenzo@hotmail.com>
2019-01-13 15:35:14 +0100

    Iniziato il gioco di tris

--- TicTacToe.py
+++ TicTacToe.py
@@ -13,41 +13,41 @@ header = ttk.Label(root, text="Benvenuto su TicTacToe!", font='Helvetica 20 bold
 header.grid(row=0, column=0)
 
 
-Frame_0 = Frame(root)
-Frame_0.grid(row=1, column=0, columnspan=10, rowspan=10)
-#tkinter.ttk.Separator(root, orient=VERTICAL).grid(column=1, row=2, rowspan=3, sticky=NS)
+Frame_0 = Frame(root, width=30, height=30, bg="green")
+Frame_0.grid(row=1, column=0)
+#tkinter.ttk.Separator.grid(column=1, row=2, rowspan=3, sticky=NS)
 
-Frame_1 = Frame(root)
-Frame_1.grid(row=1, column=1, columnspan=10, rowspan=10)
-#tkinter.ttk.Separator(root, orient=VERTICAL).grid(column=1, row=2, rowspan=3, sticky=NS)
+Frame_1 = Frame(root, width=30, height=30, bg="red")
+Frame_1.grid(row=1, column=1)
+#tkinter.ttk.Separator.grid(column=1, row=2, rowspan=3, sticky=NS)
 
-Frame_2 = Frame(root)
-Frame_2.grid(row=1, column=2, columnspan=10, rowspan=10)
-#tkinter.ttk.Separator(root, orient=VERTICAL).grid(column=1, row=2, rowspan=3, sticky=NS)
+Frame_2 = Frame(root, width=30, height=30, bg="green")
+Frame_2.grid(row=1, column=2)
+#tkinter.ttk.Separator.grid(column=1, row=2, rowspan=3, sticky=NS)
 
-Frame_3 = Frame(root)
-Frame_3.grid(row=2, column=0, columnspan=10, rowspan=10)
-#tkinter.ttk.Separator(root, orient=VERTICAL).grid(column=1, row=2, rowspan=3, sticky=NS)
+Frame_3 = Frame(root, width=30, height=30, bg="red")
+Frame_3.grid(row=2, column=0)
+#tkinter.ttk.Separator.grid(column=1, row=2, rowspan=3, sticky=NS)
 
-Frame_4 = Frame(root)
-Frame_4.grid(row=2, column=1, columnspan=10, rowspan=10)
-#tkinter.ttk.Separator(root, orient=VERTICAL).grid(column=1, row=2, rowspan=3, sticky=NS)
+Frame_4 = Frame(root, width=30, height=30, bg="green")
+Frame_4.grid(row=2, column=1)
+#tkinter.ttk.Separator.grid(column=1, row=2, rowspan=3, sticky=NS)
 
-Frame_5 = Frame(root)
-Frame_5.grid(row=2, column=2, columnspan=10, rowspan=10)
-#tkinter.ttk.Separator(root, orient=VERTICAL).grid(column=1, row=2, rowspan=3, sticky=NS)
+Frame_5 = Frame(root, width=30, height=30, bg="red")
+Frame_5.grid(row=2, column=2)
+#tkinter.ttk.Separator.grid(column=1, row=2, rowspan=3, sticky=NS)
 
-Frame_6 = Frame(root)
-Frame_6.grid(row=3, column=0, columnspan=10, rowspan=10)
-#tkinter.ttk.Separator(root, orient=VERTICAL).grid(column=1, row=2, rowspan=3, sticky=NS)
+Frame_6 = Frame(root, width=30, height=30, bg="green")
+Frame_6.grid(row=3, column=0)
+#tkinter.ttk.Separator.grid(column=1, row=2, rowspan=3, sticky=NS)
 
-Frame_7 = Frame(root)
-Frame_7.grid(row=3, column=1, columnspan=10, rowspan=10)
-#tkinter.ttk.Separator(root, orient=VERTICAL).grid(column=1, row=2, rowspan=3, sticky=NS)
+Frame_7 = Frame(root, width=30, height=30, bg="red")
+Frame_7.grid(row=3, column=1)
+#tkinter.ttk.Separator.grid(column=1, row=2, rowspan=3, sticky=NS)
 
-Frame_8 = Frame(root)
-Frame_8.grid(row=3, column=2, columnspan=10, rowspan=10)
-#tkinter.ttk.Separator(root, orient=VERTICAL).grid(column=1, row=2, rowspan=3, sticky=NS)
+Frame_8 = Frame(root, width=30, height=30, bg="green")
+Frame_8.grid(row=3, column=2)
+#tkinter.ttk.Separator.grid(column=1, row=2, rowspan=3, sticky=NS)
 
 
 
